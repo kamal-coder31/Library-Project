0879d98bf89dba14533f8ab57d9f8dd44b614185
kamal-coder31 <Kamaljot.Kaur@gisma-student.com>
2026-06-16 17:48:26 +0000

    first commit

--- book.py
+++ book.py
@@ -1,1 +1,12 @@
+class Book:
+    def __init__(self,title,author):
+      self.title = title
+      self.author = author
+      self.is_available = True
+    
+    def __str__(self):
+        print(self.title, +" "+, self.author)
+
+
+
 
