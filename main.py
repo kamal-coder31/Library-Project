fa4ffa430e03b0bd910a21e76a4fa1bb39f7febe
kamal-coder31 <Kamaljot.Kaur@gisma-student.com>
2026-06-19 17:17:52 +0000

    added user class and updated main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from book import Book
 from library import Library
+from user import User
 
 lib = Library()
+
+user1 = User("Kamal")
 
 book1 = Book("Harry Potter", "J.K. Rowling")
 book2 = Book ("The A1chemist", "Pualo Coelho")
@@ -9,4 +12,7 @@ book2 = Book ("The A1chemist", "Pualo Coelho")
 lib.add_book(book1)
 lib.add_book(book2)
 
+user1.borrow_book(book1)
+
 lib.show_books()
+user1.show_user_books()
--- user.py
+++ user.py
@@ -1,1 +1,16 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.borrowed_books = []
 
+    def borrow_book(self,book):
+        self.borrowed_books.append(book)
+
+    def return_book(self, book):
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+    
+    def show_user_books(self):
+       print(f"{self.name} has borrowed:")
+       for book in self.borrowed_books:
+          print(book)
