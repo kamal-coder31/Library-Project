24da304bc6126cb17ef7304876bc9b9a3253afda
kamal-coder31 <Kamaljot.Kaur@gisma-student.com>
2026-06-19 18:30:52 +0000

    updated main.py

--- borrow.py
+++ borrow.py
@@ -1,1 +1,7 @@
+class Borrow:
+    def __init__(self, user, book):
+        self.user = user
+        self.book = book
 
+    def show_borrow_info(self):
+        print(self.user.name, "borrowed", self.book.book)
--- main.py
+++ main.py
@@ -1,18 +1,56 @@
 from book import Book
 from library import Library
 from user import User
+from borrow import Borrow
 
-lib = Library()
-
+library = Library()
 user1 = User("Kamal")
 
-book1 = Book("Harry Potter", "J.K. Rowling")
-book2 = Book ("The A1chemist", "Pualo Coelho")
+while True:
+    print("\n===== LIBRARY MENU =====")
+    print("1. Add Book")
+    print("2. Show books")
+    print("3. Borrow Book")
+    print("4. Show user books")
+    print("5. Exit")
 
-lib.add_book(book1)
-lib.add_book(book2)
+    choice = input("Enter choice: ")
 
-user1.borrow_book(book1)
+    #1. ADD BOOK
+    if choice == "1":
+        title = input("Enter book title: ")
+        author = input("Enter author: ")
+        book = Book(title, author)
+        library.add_book(book)
+        print("Book added successfully!")
 
-lib.show_books()
-user1.show_user_books()
+    # 2.SHOW BOOKS
+    elif choice == "2":
+     library.show_books()
+
+    # 3.BORROW BOOK
+    elif choice == "3":
+       title = input ("Enter book title to borrow: ")
+       for book in library.books:
+          if book.title == title:
+             borrow = Borrow(user1, book)
+             user1.borrow_book(book)
+             print("Book Borrowed!")
+             break
+          else:
+             print("Book not found!")
+
+    # 4. SHOW USER BOOKS
+    elif choice == "4":
+       user1.show_user_books()
+
+    # 5. EXIT
+    elif choice == "5":
+        print("Exiting system...")
+        break
+
+    else:
+       print("Invalid choice!")
+          
+
+
