07a5e05d43bdd0d0173aab2ec141e146c2634a70
kamal-coder31 <Kamaljot.Kaur@gisma-student.com>
2026-06-24 14:37:36 +0000

    updated

--- library.py
+++ library.py
@@ -8,3 +8,18 @@ class Library:
     def show_books(self):
         for book in self.books:
             print(book)
+        
+    def save_books(self):
+        with open ("books.text", "w") as file:
+            for book in self.books:
+                file.write(book.title + "," + book.author + "\n")
+
+    def load_books(self):
+        try:
+            with open("books.txt", "r") as file:
+                for line in file:
+                    title, author = line.strip().split(",")
+
+                    self.books.append(self.books(title, author))
+        except FileNotFoundError:
+         self.books = []
